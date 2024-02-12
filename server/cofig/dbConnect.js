425610fc278fa05876afc0c8b5dd74bd0beb2dec
Sara YarAhmadi <sarayarahmadi69@gmail.com>
2024-02-12 23:15:05 +0330

    FEATURE:Add DbConnect

---
+++ server/cofig/dbConnect.js
@@ -0,0 +1,20 @@
+
+import mongoose from 'mongoose'
+
+const dbConnect = () => {
+    try {
+        mongoose.connect(process.env.MONGODB_URL);
+
+        mongoose.connection.once("open", () => {
+            console.log("mongoose connections is successful");
+        });
+    } catch (error) {
+        console.log("database error");
+    }
+}
+
+
+export default dbConnect
+
+
+
