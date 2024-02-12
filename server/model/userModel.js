75e3c413f2ff7edd348f5f22f952247a28b81aeb
Sara YarAhmadi <sarayarahmadi69@gmail.com>
2024-02-13 00:08:29 +0330

    REFACTOR:Change UserModel

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -1,13 +1,60 @@
 import mongoose from 'mongoose'
+import bcrypt from "bcrypt";
 
+// Declare the Schema of the Mongo model
 const userSchema = new mongoose.Schema(
   {
-    username:  String,
-    phone: String,
-    email:String,
-    password: String,
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      // unique: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      // unique: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    cart: {
+      type: Array,
+      default: [],
+    },
+    address: {
+      type: String,
+    },
+    refreshToken: {
+      type: String,
+    },
   },
-);
-const userModel = mongoose.model('User', userSchema);
-export default userModel;
+  {
+    timestamps: true,
+  });
 
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSaltSync(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const userModel = mongoose.model('User', userSchema)
+//Export the model
+
+export default userModel;
